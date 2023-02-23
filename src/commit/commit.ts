e525affa896ce098dc24a929d5039bced446e7d0
suicxyz <suicxyz@proton.me>
2023-02-23 20:01:05 -0300

    :sparkles: feat: add commit function
    commit function with possibility to push files to default or another branch

--- src/commit/commit.ts
+++ src/commit/commit.ts
@@ -1,14 +1,36 @@
+import { execSync } from "child_process";
+
 import getEmoji from "./get-emoji";
 import chalk from "chalk";
 
-type Commit = {
-	files: string[];
+export type Commit = {
+	files: string;
 	message: string;
+	commitType: string;
+	keyword: string;
 	description?: string;
 	defaultBranch: boolean;
 	branch?: string;
 };
 
 export default (params: Commit): string => {
-	return "";
+	execSync(`git add ${params.files}`);
+	const emoji = getEmoji(params.keyword, params.commitType);
+	const hasDescription =
+		params.description === undefined ? "" : ` -m "${params.description}"`;
+
+	execSync(
+		`git commit -m "${emoji} ${params.keyword}: ${params.message}"${hasDescription}`
+	);
+
+	if (params.defaultBranch) {
+		execSync("git push");
+
+		return "Changes commited to default branch.";
+	} else if (!params.defaultBranch && !params.branch) {
+		return "No branch selected to commit changes.";
+	} else if (!params.defaultBranch && params.branch) {
+		execSync(`git push -u ${params.branch}`);
+		return `Changes commited to branch "${params.branch}".`;
+	}
 };
