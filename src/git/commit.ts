351f55276a5d2f1fffc167c923d6addce371bc40
suicxyz <suicxyz@proton.me>
2023-02-27 15:53:26 -0300

    :buld: docs: removing comments

--- src/git/commit.ts
+++ src/git/commit.ts
@@ -16,30 +16,8 @@ export const commit = () => {
 		answers.commitMessage,
 		answers.addLongDescription ? answers.description : undefined
 	);
-	// const stdout = pushCommits(answers.useDefaultBranch, answers.useBranch);
-
+	
 	console.log(ADDstdout);
 	console.log(MSGstdout);
 };
 
-// export default (params: Commit): string => {
-// 	execSync(`git add ${params.files}`);
-
-// 	const hasDescription =
-// 		params.description === undefined ? "" : ` -m "${params.description}"`;
-
-// 	execSync(
-// 		`git commit -m "${emoji} ${params.keyword}: ${params.message}"${hasDescription}`
-// 	);
-
-// 	if (params.defaultBranch) {
-// 		execSync("git push");
-
-// 		return "Changes commited to default branch.";
-// 	} else if (!params.defaultBranch && !params.branch) {
-// 		return "No branch selected to commit changes.";
-// 	} else if (!params.defaultBranch && params.branch) {
-// 		execSync(`git push -u ${params.branch}`);
-// 		return `Changes commited to branch "${params.branch}".`;
-// 	}
-// };
