f9ff5b05eb81ea34582633037ffc83fe61ff6f49
suicxyz <suicxyz@proton.me>
2023-02-27 15:40:36 -0300

    :lipstick: feat: change colors and line breaking

--- src/misc/greetings.ts
+++ src/misc/greetings.ts
@@ -5,14 +5,35 @@ import { getFiles } from "../git/files.js";
 export const greetings = () => {
 	console.log(
 		chalk.bold("Committer CLI") +
-			" - A cool CLI for cool commits on GitHub\nThe following files are not commited:"
+			" - A cool CLI for cool commits on GitHub\n\nThe following files are not commited:\n"
 	);
 
 	const { modifiedFiles, deletedFiles, untrackedFiles } = getFiles();
 
-	for (let i of modifiedFiles) console.log("Modified:", chalk.yellowBright(i));
-	for (let i of deletedFiles) console.log("Deleted:", chalk.redBright(i));
-	for (let i of untrackedFiles) console.log("Untracked:", chalk.cyanBright(i));
+	for (let i of modifiedFiles)
+		console.log(
+			chalk.yellowBright(
+				`${
+					chalk.bold("Modified")
+				}: ${i}`
+			)
+		);
+	for (let i of deletedFiles) 
+		console.log(
+			chalk.redBright(
+				`${
+					chalk.bold("Deleted")
+				}: ${i}`
+			)
+		);
+	for (let i of untrackedFiles)
+		console.log(
+			chalk.cyanBright(
+				`${
+					chalk.bold("Untracked")
+				}: ${i}`
+			)
+		);
 
 	console.log();
 };
